0eb90ebee8984a0abcdaa5ec651981fe99fbe5f9
iths-janna-dahlberg <janna.dahlberg@gmail.com>
2019-11-13 17:29:27 +0100

    commit new javascript

--- js/script.js
+++ js/script.js
@@ -1,67 +1,117 @@
 $(document).ready(function() {
+  /* For the sticky navigation */
 
-    /* For the sticky navigation */
+  $(".js--section-photos").waypoint(
+    function(direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "60px;"
+    }
+  );
 
-    $('.js--section-photos').waypoint(function(direction) {
-        if (direction == "down") {    
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
+  /* Navigation scroll */
+
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        let target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            1000
+          );
+          return false;
         }
-    }, {
-        offset: '60px;'
+      }
     });
+  });
 
-    /* Navigation scroll */
+  /* Animation in scroll */
 
-    $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html, body').animate({
-                scrollTop: target.offset().top
-              }, 1000);
-              return false;
-            }
-          }
-        });
-      });
+  $(".js--wp-1").waypoint(
+    function(direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%"
+    }
+  );
 
-      /* Animation in scroll */
+  $(".js--wp-2").waypoint(
+    function(direction) {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%"
+    }
+  );
 
-      $('.js--wp-1').waypoint(function(direction) {
-          $('.js--wp-1').addClass('animated fadeIn');
-      }, {
-          offset: '50%'
-      });
+  /* Mobile nav */
 
-      $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    });
+  $(".js--nav-icon").click(function() {
+    let nav = $(".js--main-nav");
+    let icon = $(".js--nav-icon i");
 
-    /* Mobile nav */
-
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-
-        nav.slideToggle(200);
-        if (icon.hasClass('ion-ios-menu')) {
-            icon.addClass('ion-ios-close');
-            icon.removeClass('ion-ios-menu');
-        } else {
-            icon.addClass('ion-ios-menu');
-            icon.removeClass('ion-ios-close');
-        }
-    });
-
+    nav.slideToggle(200);
+    if (icon.hasClass("ion-ios-menu")) {
+      icon.addClass("ion-ios-close");
+      icon.removeClass("ion-ios-menu");
+    } else {
+      icon.addClass("ion-ios-menu");
+      icon.removeClass("ion-ios-close");
+    }
+  });
 });
 
+/* pop-up img index.html */
 
-/* pop-up index gallary */
+let openModalImages = document.querySelectorAll("[data-modal-target]");
+let closeModalImages = document.querySelectorAll("[data-close-button]");
+let overlay = document.getElementById("overlay");
 
-let popUp 
+openModalImages.forEach(img => {
+  img.addEventListener("click", () => {
+    let modal = document.querySelector(img.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+closeModalImages.forEach(img => {
+  img.addEventListener("click", () => {
+    let modal = img.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+overlay.addEventListener("click", () => {
+  let modals = document.querySelectorAll(".modal.active");
+  modals.forEach(modal => {
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
